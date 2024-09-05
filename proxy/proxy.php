9beb8136fee7c1ce8b3cbe607a07c8972ea31fe3
Phil <batroff@systopia.de>
2024-09-05 09:22:57 +0200

    1.0.0-alpha2

--- proxy/proxy.php
+++ proxy/proxy.php
@@ -8,7 +8,7 @@
 +---------------------------------------------------------*/
 
 require_once "config.php";
-$civiproxy_version = '1.0.0-alpha';
+$civiproxy_version = '1.0.0-alpha2';
 
 /**
  * this will redirect the request to another URL,
