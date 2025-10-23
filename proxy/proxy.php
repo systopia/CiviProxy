3092a57752718b8cc39a4f481e559a9eb00553e3
Jens Schuppe <schuppe@systopia.de>
2025-10-23 10:41:53 +0200

    Back to dev (1.1.0-dev)

--- proxy/proxy.php
+++ proxy/proxy.php
@@ -8,7 +8,7 @@
 +---------------------------------------------------------*/
 
 require_once "config.php";
-$civiproxy_version = '1.0.0-beta';
+$civiproxy_version = '1.1.0-dev';
 
 /**
  * this will redirect the request to another URL,
